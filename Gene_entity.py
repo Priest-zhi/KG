bb3287be42181545e4b5d1f8c5d51b3581a2dd05
quz <guiyucf123@163.com>
2020-04-14 10:23:47 +0800

    add gene entity

--- Gene_entity.py
+++ Gene_entity.py
@@ -1,14 +1,23 @@
 import csv
 from py2neo import Graph, Node, Relationship
 
-def GTF2GENE_entity(file='/data/gtf.csv'):
+def GTF2GENE_entity(file='data/gtf.csv'):
+    nodecount=0
+    GENEgraph = Graph("http://localhost:7474")
     with open(file, 'r') as fr:
         reader = csv.DictReader(fr)
         for line in reader:
             if not line:
                 break
             if line["feature"] == "gene":
-            rcon2.lpush(line['Value'].strip(), line['SO ID']+'|'+ line['SO Term'])
+                GeneNode = Node("GENE", **line)
+                GENEgraph.create(GeneNode)
+
+                nodecount+=1
+                if nodecount%100 ==0:
+                    print(nodecount)
+
 
 if __name__ == '__main__':
-    pass
+    GTF2GENE_entity()
+    print("ALL done!")
