c2856f284fb6d907b50248959eeeccdc3c318f85
quz <guiyucf123@163.com>
2020-04-15 15:57:12 +0800

    1. change to obo function

--- OBO_entity.py
+++ OBO_entity.py
@@ -1,8 +1,11 @@
 import csv
 from py2neo import Graph, Node, Relationship
 
-def CreateHPOEntity(file = "data/hp.obo"):
-    HPgraph = Graph("http://localhost:7474")
+OBOArrayField=['xref','synonym','is_a','alt_id','intersection_of','relationship']
+#input file: xxx.obo
+#input type: HPO, DO, SO, GO
+def CreateOBOEntity(file, type):
+    OBOgraph = Graph("http://localhost:7474")
     nodecount = 0
     with open(file, 'r') as fr:
         while True:
@@ -20,7 +23,8 @@ def CreateHPOEntity(file = "data/hp.obo"):
 
                     if strtmp[1].strip().startswith('"'):
                         strtmp[1]=strtmp[1].strip().split('"')[1]
-                    if strtmp[0] == 'xref' or strtmp[0] == 'synonym' or strtmp[0] == 'is_a' or strtmp[0] == 'alt_id': #array
+                    if strtmp[0] in OBOArrayField:
+                    #if strtmp[0] == 'xref' or strtmp[0] == 'synonym' or strtmp[0] == 'is_a' or strtmp[0] == 'alt_id': #array
                         if strtmp[0].strip() in node:
                             node[strtmp[0].strip()].append(strtmp[1].strip())
                         else:
@@ -28,37 +32,40 @@ def CreateHPOEntity(file = "data/hp.obo"):
                     else:
                         node[strtmp[0].strip()] = strtmp[1].strip()
                 #create node
-                exitNodelist = list(HPgraph.nodes.match("HPO", id=node['id']))
-                if len(exitNodelist) > 1:
-                    print("error, search more node ", node['id'])
-                    exit()
-                if len(exitNodelist) == 1:
+                exitNodelist = list(OBOgraph.nodes.match(type, id=node['id']))
+                # if len(exitNodelist) > 1:
+                #     print("error, search more node ", node['id'])
+                #     exit()
+                if len(exitNodelist) >= 1:
                     exitNode=exitNodelist[0]
                     for ele in node:
                         exitNode[ele]=node[ele]
-                    HPgraph.push(exitNode)
+                    OBOgraph.push(exitNode)
                     currentNode=exitNode
                 else:
-                    HPNode = Node("HPO",**node)
-                    HPgraph.create(HPNode)
-                    currentNode=HPNode
+                    NewNode = Node(type,**node)
+                    OBOgraph.create(NewNode)
+                    currentNode=NewNode
                 #create relationship
                 if 'is_a' in node:
                     for childele in node['is_a']:
                         childid = childele.split(' ! ')[0].strip()
-                        childExitNode = HPgraph.nodes.match("HPO", id=childid).first()
+                        childExitNode = OBOgraph.nodes.match(type, id=childid).first()
                         if childExitNode:
                             childrelation = Relationship(currentNode, 'father', childExitNode)
                         else:
-                            HPNodeEnd = Node("HPO", id=childid)
-                            childrelation = Relationship(currentNode,'father',HPNodeEnd)
-                        HPgraph.create(childrelation)
+                            NewNodeEnd = Node(type, id=childid)
+                            childrelation = Relationship(currentNode,'father',NewNodeEnd)
+                        OBOgraph.create(childrelation)
 
                 nodecount+=1
                 if nodecount%100==0:
                     print(nodecount)
 
+
+
 if __name__ == '__main__':
-    #CreateHPOEntity("data/hptext")
-    CreateHPOEntity()
+    #CreateOBOEntity("data/so.obo","SO")
+    #CreateOBOEntity("data/doid.obo", "DO")
+    CreateOBOEntity("data/go.obo", "GO")
     print("ALL done!")
